f6d0371e7085294fb17893d7f4dd5b482c845f3e
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-02-22 04:28:06 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 sed -i "s/host = localhost/host = dpg-d6d0m8ktgctc73es4c80-a/" /var/www/html/config.inc.php
 sed -i "s/username = ojs/username = ojsuser/" /var/www/html/config.inc.php
-sed -i "s/password = ojs/password = YOUR_PASSWORD/" /var/www/html/config.inc.php
+sed -i "s/password = ojs/password = FpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW/" /var/www/html/config.inc.php
 sed -i "/^\[database\]/,/^\[/ s/^name =.*/name = ojs_db/" /var/www/html/config.inc.php
 sed -i "s/installed = On/installed = Off/" /var/www/html/config.inc.php
 
