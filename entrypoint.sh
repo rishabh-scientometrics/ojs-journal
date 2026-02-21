c503bc784925f691c4b37f738ffa675a53873a33
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-02-22 03:20:01 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sed -i "s/host = localhost/host = $OJS_DB_HOST/" /var/www/html/config.inc.php
+sed -i "s/port = 3306/port = ${OJS_DB_PORT:-5432}/" /var/www/html/config.inc.php
+sed -i "s/username = ojs/username = $OJS_DB_USER/" /var/www/html/config.inc.php
+sed -i "s/password = ojs/password = $OJS_DB_PASSWORD/" /var/www/html/config.inc.php
+sed -i "s/name = ojs/name = $OJS_DB_NAME/" /var/www/html/config.inc.php
+
+exec /usr/local/bin/ojs-start
