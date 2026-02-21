9353c6d7f722fe7ec870c66b77757eb950e2d838
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-02-22 04:09:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+# Replace database config using exact OJS template defaults
 sed -i "s/host = localhost/host = $OJS_DB_HOST/" /var/www/html/config.inc.php
-sed -i "s/port = 3306/port = ${OJS_DB_PORT:-5432}/" /var/www/html/config.inc.php
+sed -i "s/port = 5432/port = ${OJS_DB_PORT:-5432}/" /var/www/html/config.inc.php
 sed -i "s/username = ojs/username = $OJS_DB_USER/" /var/www/html/config.inc.php
 sed -i "s/password = ojs/password = $OJS_DB_PASSWORD/" /var/www/html/config.inc.php
 sed -i "s/name = ojs/name = $OJS_DB_NAME/" /var/www/html/config.inc.php
 
-# Run PKP pre-start if it exists
-if [ -f /usr/local/bin/pkp-prestart.sh ]; then
-    /usr/local/bin/pkp-prestart.sh
-fi
+# Force-set any remaining localhost/default values
+sed -i "/^\[database\]/,/^\[/ s/^port =.*/port = ${OJS_DB_PORT:-5432}/" /var/www/html/config.inc.php
+sed -i "/^\[database\]/,/^\[/ s/^name =.*/name = $OJS_DB_NAME/" /var/www/html/config.inc.php
 
-grep -A5 "\[database\]" /var/www/html/config.inc.php
+grep -A8 "\[database\]" /var/www/html/config.inc.php
+
 exec apache2-foreground
