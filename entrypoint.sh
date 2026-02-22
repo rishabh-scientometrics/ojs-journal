8b4aaefb804af5f8ab0649e2f574e636e08d06a7
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-02-22 15:31:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,39 @@
 
 chmod -R 777 /var/www/html/cache /var/www/html/public /var/www/files
 
-# Fix config using PHP with more precise patterns
+# Read config and rewrite only the [database] section cleanly
 php -r "
 \$config = file_get_contents('/var/www/html/config.inc.php');
-\$config = preg_replace('/^driver = .*/m', 'driver = postgres', \$config);
-\$config = preg_replace('/^host = .*/m', 'host = dpg-d6d0m8ktgctc73es4c80-a', \$config);
-\$config = preg_replace('/^port = .*/m', 'port = 5432', \$config);
-\$config = preg_replace('/^username = .*/m', 'username = ojsuser', \$config);
-\$config = preg_replace('/^password = .*/m', 'password = FpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW', \$config);
-\$config = preg_replace('/^name = .*/m', 'name = ojs_db', \$config);
-\$config = preg_replace('/^installed = On/m', 'installed = Off', \$config);
+\$config = preg_replace(
+    '/(\[database\].*?driver = )[\w]+/s',
+    '\${1}postgres',
+    \$config
+);
+\$config = preg_replace(
+    '/(\[database\].*?host = )[\w\.\-]+/s',
+    '\${1}dpg-d6d0m8ktgctc73es4c80-a',
+    \$config
+);
+\$config = preg_replace(
+    '/(\[database\].*?\nusername = )[\w]+/s',
+    '\${1}ojsuser',
+    \$config
+);
+\$config = preg_replace(
+    '/(\[database\].*?\npassword = )[\w]+/s',
+    '\${1}FpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW',
+    \$config
+);
+\$config = preg_replace(
+    '/(\[database\].*?\nname = )[\w]+/s',
+    '\${1}ojs_db',
+    \$config
+);
+\$config = str_replace('installed = On', 'installed = Off', \$config);
 file_put_contents('/var/www/html/config.inc.php', \$config);
 "
 
-grep -A10 "\[database\]" /var/www/html/config.inc.php
+grep -A12 "\[database\]" /var/www/html/config.inc.php
 
 cd /var/www/html
 printf 'en\n\n/var/www/files\nadmin\nAdmin1234!\nAdmin1234!\nadmin@example.com\npostgres9\ndpg-d6d0m8ktgctc73es4c80-a\n5432\nojs_db\nojsuser\nFpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW\n' | su -s /bin/bash www-data -c "php -d auto_prepend_file='' tools/install.php"
