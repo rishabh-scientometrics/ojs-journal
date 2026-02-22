ae4436eeb9759e6faebf04df2c507c2d7c9b0e5f
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-02-22 14:46:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,14 @@
 
 sed -i "s/host = localhost/host = dpg-d6d0m8ktgctc73es4c80-a/" /var/www/html/config.inc.php
 sed -i "s/username = ojs/username = ojsuser/" /var/www/html/config.inc.php
-sed -i "s/password = ojs/password = FpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW/" /var/www/html/config.inc.php
+sed -i "s/password = ojs/password = YOUR_PASSWORD/" /var/www/html/config.inc.php
 sed -i "/^\[database\]/,/^\[/ s/^name =.*/name = ojs_db/" /var/www/html/config.inc.php
 sed -i "s/installed = On/installed = Off/" /var/www/html/config.inc.php
 
-# Fix permissions before running installer
 chmod -R 777 /var/www/html/cache /var/www/html/public /var/www/files
 
-# Run CLI installer as www-data
-# Run CLI installer as www-data
 cd /var/www/html
-echo "en
-IN
-rishabhs03
-Punjab101
-rishabh.scientometrics@gmail.com
-postgres
-dpg-d6d0m8ktgctc73es4c80-a
-5432
-ojs_db
-ojsuser
-FpgX7WWDWxhqRXnEg6E4QTVIxM1fBsuW
-/var/www/files
-" | su -s /bin/bash www-data -c "php tools/install.php"
+printf 'en\n\n/var/www/files\nadmin\nAdmin1234!\nAdmin1234!\nadmin@example.com\npostgres\ndpg-d6d0m8ktgctc73es4c80-a\n5432\nojs_db\nojsuser\nYOUR_PASSWORD\n' | su -s /bin/bash www-data -c "php -d auto_prepend_file='' tools/install.php"
 
 echo "=== Installer exit code: $? ==="
 
