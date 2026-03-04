d668a177f44c79d15aa7eaf12c131f6ffb20a39d
rishabh-scientometrics <rishabh.scientometrics@gmail.com>
2026-03-04 22:24:05 +0530

    Update patch.php

--- patch.php
+++ patch.php
@@ -2,9 +2,9 @@
 $file = '/var/www/html/lib/pkp/classes/core/PKPApplication.php';
 $content = file_get_contents($file);
 $content = preg_replace(
-    '/public static function isInstalled\(\).*?\{.*?return[^}]+\}/s',
-    'public static function isInstalled(): bool { return false; }',
+    '/public static function isInstalled\(\): bool \{ return false; \}/',
+    'public static function isInstalled(): bool { return (bool) Config::getVar(\'general\', \'installed\'); }',
     $content
 );
 file_put_contents($file, $content);
-echo "isInstalled patched\n";
+echo "isInstalled patched to read from config\n";
